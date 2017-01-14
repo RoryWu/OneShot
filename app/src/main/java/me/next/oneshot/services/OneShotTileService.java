177cd768d124b6bd369417bbc462d23e1cc2ec06
NeXT___ <kingideayou@foxmail.com>
2017-01-14 11:34:54 +0800

    feature: 添加 TileService,点击 Tile 时隐藏 Notification Center

--- app/src/main/java/me/next/oneshot/services/OneShotTileService.java
+++ app/src/main/java/me/next/oneshot/services/OneShotTileService.java
@@ -1,8 +1,11 @@
 package me.next.oneshot.services;
 
+import android.content.Intent;
 import android.os.Build;
 import android.service.quicksettings.TileService;
 import android.support.annotation.RequiresApi;
+
+import me.next.oneshot.ScreenShotActivity;
 
 /**
  * Created by NeXT on 17/1/10.
@@ -34,6 +37,15 @@ public class OneShotTileService extends TileService {
     @Override
     public void onClick() {
         super.onClick();
+
+        // 收起系统 Notification Center
+        // see: http://stackoverflow.com/questions/15568754/how-to-close-the-status-bar-notification-panel-after-notification-button-click
+        Intent it = new Intent(Intent.ACTION_CLOSE_SYSTEM_DIALOGS);
+        getApplicationContext().sendBroadcast(it);
+
+        Intent intent = new Intent(getApplicationContext(), ScreenShotActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
     }
 
 }
